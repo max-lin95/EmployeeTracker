868199c05d8d14e0214f8bcc0f80a816f5a3f63f
Jeremy Lin <maxlinjr95@gmail.com>
2022-10-09 13:55:26 -0700

    prompt start

--- index.js
+++ index.js
@@ -18,3 +18,48 @@ connection.connect(function(err) {
     afterConnection();
 });
 
+// Prompt
+function promptStart() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choices",
+            choies: [
+               "View all roles", 
+               "View all employees",
+               "Add a department",
+               "Add a role",
+               "Add an employee",
+               "Update an employee role",
+            ]
+        }
+    ]).then(function(val) {
+        switch(val.choice) {
+            case "View all employee roles?":
+                viewAllRoles();
+                break;
+
+            case "View all employees?":
+                viewAllEmployees();
+                break;
+            
+            case "Add a department?":
+                addDepartment();
+                break;
+
+            case "Add a role?":
+                addRole();
+                break;
+
+            case "Add an employee?":
+                addEmployee();
+                break;
+
+            case "Update an employee role?":
+                addEmployeeRole();
+                break;
+        }
+    })
+}
+
