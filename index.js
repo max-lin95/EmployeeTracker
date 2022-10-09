4bc93e2850b106f683492a16cf18395fe0164f80
Jeremy Lin <maxlinjr95@gmail.com>
2022-10-09 13:41:15 -0700

    connection and connect error added

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // Dependencies 
 const inquirer = require("inquirer")
 const mysql = require("mysql")
-const cTable = require('console.table');
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "DBpassword",
+    database: "employeeTrackerDB",
+});
+
+// Connect Error
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as ID" + connection.threadId);
+    afterConnection();
+});
 
